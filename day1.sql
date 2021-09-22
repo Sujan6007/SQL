ce7126ea12f702862316cd2235b1bbdc5affac3d
Sujan6007 <90595888+Sujan6007@users.noreply.github.com>
2021-09-22 11:13:03 +0530

    Add files via upload

---
+++ day1.sql
@@ -0,0 +1,16 @@
+ show databases;  
+  create database students;
+
+ use students;
+ show tables;
+ 
+drop table students;
+ 
+select * from stud_table s where s.name='sujan';
+
+ insert into stud_table (id,name,marks,dept_id)
+ values(1,'sujan',520,1),(2,'ramu',700,1),(3,'mahesh',650,2),(4,'raju',900,2),(5,'vijay',850,3);
+
+ select count(*) from stud_table;
+ 
+select * from stud_table group by marks;
---
+++ day2.sql
@@ -0,0 +1,57 @@
+ insert into dept(id,dept_name)
+  values(1,'social'),(2,'science'),(3,'electronics');
+
+  select * from dept;
+ select s.* from dept d, stud_table s where s.dept_id=1;
+ 
+select s.* from dept d, stud_table s where s.marks>500 and d.dept_name='maths' and s.dept_id=d.id;
+
+alter table stud_table add result varchar(50);
+
+alter table stud_table modify result int;
+ 
+alter table stud_table drop result;
+  
+ delete from dept where dept_name='social';
+ 
+  delete from dept where id=1 and dept_name='social' ;
+ 
+  truncate table dept;
+ 
+  select * from employee;
+ 
+  insert into employee(emp_name,salary)
+   values('Sujan',26000),('Mahesh',27000),('Srinivas',30000),('Ramu',32000);
+
+   truncate table employee;
+ 
+  select * from stud_table;
+
+   update stud_table set marks=1000 where id=1;
+   
+
+   create table sales(
+   salesID int not null primary key ,
+   salesName varchar(50),
+   adress varchar(50)
+   );
+   
+  
+ create table orders_sales(
+   id int primary key ,
+   productName varchar(50),
+   adress varchar(50),
+   salesID Int,
+   constraint FK_order foreign key(salesID) references sales(salesID)
+   );
+  
+ insert into sales(salesID,salesName,adress)
+   values(1,'Sujan','Shimoga'),(2,'Mahesh','Hyderabad'),(3,'Srinivas','Bangalore'),(4,'Ramu','Bangalore');
+  
+ insert into orders_sales(id,productName,adress,salesID)
+   values(1,'Fogg','Shimoga',1),(2,'Apple','Bangalore',2),(3,'acer','Hyderabad',1);
+
+   select * from sales join orders_sales;
+   select * from orders_sales;
+    select * from sales where salesID in (select id from orders_sales where productName = 'Fogg' || adress='Hyderabad' );
+    select * from sales;
